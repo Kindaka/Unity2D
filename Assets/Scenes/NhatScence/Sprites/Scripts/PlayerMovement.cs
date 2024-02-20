d7cda0a960e765bd804be3c720f459e62fa36ee9
NhatNguyen <nhatnguyen45678@gmail.com>
2024-02-20 16:25:42 +0700

    NhatScene

--- Assets/Scenes/NhatScence/Sprites/Scripts/PlayerMovement.cs
+++ Assets/Scenes/NhatScence/Sprites/Scripts/PlayerMovement.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,8 +15,10 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float jumpForce = 14f;
 
-    private enum MovementState { idle, running, jumping, falling}
-    private MovementState state = MovementState.idle;
+    private enum MovementState { idle, running, jumping, falling }
+
+    [SerializeField] private AudioSource jumpSoundEffect;
+
     // Start is called before the first frame update
     private void Start()
     {
@@ -29,18 +31,19 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         dirX = Input.GetAxisRaw("Horizontal");
+        dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
-       
+
 
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
+            jumpSoundEffect.Play();
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
-        
+
         UpdateAnimationState();
 
-    
+
     }
 
     private void UpdateAnimationState()
@@ -61,7 +64,7 @@ public class PlayerMovement : MonoBehaviour
         {
             state = MovementState.idle;
         }
-        if(rb.velocity.y > .1f)
+        if (rb.velocity.y > .1f)
         {
             state = MovementState.jumping;
         }
@@ -69,10 +72,10 @@ public class PlayerMovement : MonoBehaviour
         {
             state = MovementState.falling;
         }
-        anim.SetInteger("state",(int)state);
+        anim.SetInteger("state", (int)state);
     }
     private bool IsGrounded()
     {
-        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f,jumpableGround);
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
