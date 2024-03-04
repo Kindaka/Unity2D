36ea98ec39d42279020a2d3623de1ef4f114f8af
TuanLoc <121934071+ZiccoMartinez@users.noreply.github.com>
2024-03-05 05:30:30 +0700

    Add files via upload

---
+++ Assets/Scripts/MenuEvents.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuEvents : MonoBehaviour
+{
+    public void ExitButton()
+    {
+        Application.Quit();
+        Debug.Log("Game Closed");
+    }
+
+   public void LoadMap(int index)
+    {
+        SceneManager.LoadScene(0);
+   }
+}
